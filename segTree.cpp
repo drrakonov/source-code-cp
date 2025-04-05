0e893d2702571c6547d608b02c1675537bae84d3
Abhay Pratap Singh <138102159+drrakonov@users.noreply.github.com>
2025-04-05 20:50:44 +0530

    Update segTree.cpp
    Adding segment tree using lazy propogation

--- segTree.cpp
+++ segTree.cpp
@@ -49,3 +49,87 @@ public:
         seg[ind] = min(seg[ind * 2 + 1], seg[ind * 2 + 2]);
     }
 };
+
+
+
+
+
+
+//segment tree for range query using lazy propogation technique
+class SEGTreeLazy{
+    vector<int> seg, lazy;
+public:
+    SEGTreeLazy(int n) {
+        seg.resize(4 * n);
+        lazy.resize(4 * n);
+    };
+public:
+    void build(int ind, int low, int high, int nums[]) {
+        if(low == high) {
+            seg[ind] = nums[low];
+            return;
+        }
+
+        int mid = (low + high) >> 1;
+        build(ind * 2 + 1, low, mid, nums);
+        build(ind * 2 + 2, mid + 1, high, nums);
+        seg[ind] = seg[ind * 2 + 1] + seg[ind * 2 + 2];
+    }
+public:
+    void update(int ind, int low, int high, int l, int r, int val) {
+        //update the prev remaining updates && propogate downwards
+        if(lazy[ind] != 0) {
+            seg[ind] += (high - low + 1) * lazy[ind];
+            //propogate downwards for remaining elements to be updated
+            if(low != high) {
+                lazy[ind * 2 + 1] += lazy[ind];
+                lazy[ind * 2 + 2] += lazy[ind];
+            }
+            lazy[ind] = 0;
+        }
+
+        //no overlaps
+        if(r < low || l > high) return;
+
+        //complete overlap
+        if(l <= low && high <= r) {
+            seg[ind] += (high - low + 1) * val;
+            //if low != high, it means children exists
+            if(low != high) {
+                lazy[ind * 2 + 1] += val;
+                lazy[ind * 2 + 2] += val;
+            }
+            return;
+        }
+
+        //partial overlap
+        int mid = (low + high) >> 1;
+        update(ind * 2 + 1, low, mid, l, r, val);
+        update(ind * 2 + 2, mid + 1, high, l, r, val);
+        seg[ind] = seg[ind * 2 + 1] + seg[ind * 2 + 2];
+    }
+public:
+    int query(int ind, int low, int high, int l, int r) {
+        //update if any updates are remaining 
+        if(lazy[ind] != 0) {
+            seg[ind] += (high - low + 1) * lazy[ind];
+            if(low != high) {
+                lazy[ind * 2 + 1] += lazy[ind];
+                lazy[ind * 2 + 2] += lazy[ind];
+            }
+            lazy[ind] = 0;
+        }
+        //no overlap
+        if(high < l || r < low) {
+            return 0;
+        }
+
+        //complete overlap
+        if(l <= low && high <= r) return seg[ind];
+
+        int mid = (low + high) >> 1;
+        int left = query(ind * 2 + 1, low, mid, l, r);
+        int right = query(ind * 2 + 2, mid + 1, high, l, r);
+        return left + right;
+    }
+};
